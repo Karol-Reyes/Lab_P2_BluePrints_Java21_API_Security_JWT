b1b8e2800855492e36f70c1e2e9f2dd0f337a745
David Moreno <davidmorenodaleman@gmail.com>
2026-09-16 15:01:46 -0500

    Revert "feature: move files from lab 4"

--- src/main/java/co/edu/eci/blueprints/filters/BlueprintsFilter.java
+++
@@ -1,7 +0,0 @@
-package edu.eci.arsw.blueprints.filters;
-
-import edu.eci.arsw.blueprints.model.Blueprint;
-
-public interface BlueprintsFilter {
-    Blueprint apply(Blueprint bp);
-}
--- src/main/java/co/edu/eci/blueprints/model/ApiResponse.java
+++
@@ -1,3 +0,0 @@
-package edu.eci.arsw.blueprints.model;
-
-public record ApiResponse<T>(int code, String message, T data) {}
